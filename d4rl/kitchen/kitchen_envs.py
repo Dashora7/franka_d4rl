0c7bbe8253f67204f60ab435039bd21c0dcb3779
dashora7 <nrdashora@gmail.com>
2024-05-07 15:38:34 -0700

    new cameras and renders

--- d4rl/kitchen/kitchen_envs.py
+++ d4rl/kitchen/kitchen_envs.py
@@ -66,7 +66,14 @@ class KitchenBase(KitchenTaskRelaxV1):
         qpos = self.init_qpos.copy()
         qpos[OBS_ELEMENT_INDICES[goalname]] = OBS_ELEMENT_GOALS[goalname]
         self.set_state(qpos, np.zeros(len(self.init_qvel)))
-        goal_obs = self.render('rgb_array', width=self.imwidth, height=self.imheight)
+        
+        # goal_obs = self.render('rgb_array', width=self.imwidth, height=self.imheight)
+        goal_obs = self.sim_robot.renderer.render_offscreen(
+            self.imwidth,
+            self.imheight,
+            camera_id=2)
+        
+        
         self.set_state(backup_qpos, backup_qvel)
         self.rendered_goal = True
         self.rendered_goal_obj = goal_obs
@@ -88,8 +95,14 @@ class KitchenBase(KitchenTaskRelaxV1):
         self.obs_dict["obj_qv"] = obj_qv
         self.obs_dict["goal"] = self.goal
         if self.image_obs:
-            img = self.render(mode="rgb_array",
-                    width=self.imwidth, height=self.imheight)[..., None]
+            img = self.sim_robot.renderer.render_offscreen(
+                self.imwidth,
+                self.imheight,
+                camera_id=2)[..., None]
+            
+            #img = self.render(mode="rgb_array",
+            #        width=self.imwidth, height=self.imheight)[..., None]
+            
             # img = img.transpose(2, 0, 1).flatten()
             
             if self.proprioception:
